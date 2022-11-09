4f5196cd9a5b013f6eead03db3499cad6d5253b0
MĐ <117772875+mil1ca@users.noreply.github.com>
2022-11-09 21:00:59 +0100

    Update Zadatak01.cpp

--- Vjezbe06/Zadatak01.cpp
+++ Vjezbe06/Zadatak01.cpp
@@ -1,2 +1,36 @@
 /*Napišite program koji od korisnika učitava neograničen broj cijelih brojeva. Nakon unosa ispišite brojeve.*/
 
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(void) {
+    
+    vector<int> poljeBrojeva;
+    char nastavi;
+    
+    do {
+        int broj;
+        cout << "Unesite broj > ";
+        cin >> broj;
+        poljeBrojeva.push_back(broj);
+        
+        cout << "Nastavi unos (d/n)? > ";
+        cin >> nastavi;
+        
+    } while (nastavi == 'd');
+    
+    cout << "Uneseni brojevi:" << endl;
+    for (int i = 0; i < poljeBrojeva.size(); i++) {
+        cout << poljeBrojeva[i];
+        if (i == poljeBrojeva.size() - 1) {
+            cout << ".";
+        } else {
+            cout << ", ";
+        }
+    }
+    
+    cout << endl;
+    return 0;
+    
+}
