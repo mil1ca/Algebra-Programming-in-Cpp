11e9c3e5b6240dee782e3fc8a7e5d63e1bc3b77f
MĐ <117772875+mil1ca@users.noreply.github.com>
2022-11-09 15:09:30 +0100

    Create Zadatak09.cpp

---
+++ Vjezbe03/Zadatak09.cpp
@@ -0,0 +1,24 @@
+/*Napišite program koji s tipkovnice učitava 1 broj i ispisuje koliko on ima znamenki.*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int broj;
+	
+	cout << "Unesite broj > ";
+	cin >> broj;
+	
+	int counter = 0;
+	
+	do {
+	    broj = broj / 10;
+	    counter++;
+	    
+	} while (broj > 0);
+	
+	cout << counter;
+  
+	return 0;
+}
