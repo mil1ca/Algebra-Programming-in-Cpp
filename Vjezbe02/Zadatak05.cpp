d879dd4876cfcd274a12fc9276fe40fecec0a58e
MĐ <117772875+mil1ca@users.noreply.github.com>
2022-11-09 13:30:04 +0100

    Create Zadatak05.cpp

---
+++ Vjezbe02/Zadatak05.cpp
@@ -0,0 +1,23 @@
+/*Napišite program koji za dani troznamenkasti broj ispisuje njegove znamenke. 
+Svaka znamenka treba biti ispisana u zasebnom retku. 
+Broj unesite direktno u programu.*/
+
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    
+    int broj = 256;
+    int lijeva, srednja, desna;
+    
+    lijeva = broj / 100;
+    srednja = (broj / 10) % 10;
+    desna = broj % 10;
+    
+    cout << "Broj: " << broj << endl;
+    cout << "Lijeva znamenka: " << lijeva << endl;
+    cout << "Srednja znamenka: " << srednja << endl;
+    cout << "Desna znamenka: " << desna << endl;
+    
+    return 0;
+}
