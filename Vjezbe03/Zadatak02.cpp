fda34d8b79d77f9f65ce218e3e24671c12dd1e79
MĐ <117772875+mil1ca@users.noreply.github.com>
2022-11-09 23:27:42 +0100

    Update Zadatak02.cpp

--- Vjezbe03/Zadatak02.cpp
+++ Vjezbe03/Zadatak02.cpp
@@ -1,5 +1,5 @@
-/*Napišite program koji s tipkovnice učitava jedan cijeli broj te ispisuje je li on djeljiv s 3 ili 6. 
-Inače ispisuje poruku “ Ucitani broj nije djeljiv niti s 3 niti s 6”.*/
+/*Napišite program koji s tipkovnice učitava jedan cijeli broj te ispisuje je li on djeljiv sa 3 ili sa 6. 
+Inače ispisuje poruku “ Ucitani broj nije djeljiv niti sa 3 niti sa 6”.*/
 
 #include <iostream>
 using namespace std;
@@ -12,13 +12,13 @@ int main() {
 	cin >> broj;
 	
 	if (broj % 3 == 0) {
-	    cout << "Broj je djeljiv s 3." << endl;
+	    cout << "Broj je djeljiv sa 3." << endl;
 	} 
 	if (broj % 6 == 0) {
-	    cout << "Broj je djeljiv s 6." << endl;
+	    cout << "Broj je djeljiv sa 6." << endl;
 	} 
 	if (broj % 3 != 00 && broj % 6 != 0) {
-	    cout << "Broj nije djeljiv ni s 3 ni s 6." << endl;
+	    cout << "Broj nije djeljiv ni sa 3 ni sa 6." << endl;
 	}
 	
 	return 0;
