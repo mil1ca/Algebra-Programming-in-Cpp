44b3ccbb82a395a6641daf98da0d2d4326a538b5
MĐ <117772875+mil1ca@users.noreply.github.com>
2022-11-09 14:20:52 +0100

    Create Primjer04.cpp

---
+++ Vjezbe02/Primjer04.cpp
@@ -0,0 +1,17 @@
+/*Deklarirajte varijablu broj koja je tipa cijeli broj, te u nju učitajte vrijednost od korisnika. 
+Ispišite varijablu broj na ekran.*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int broj;
+	
+	cout << "Unesite cijeli broj > ";
+	cin >> broj;
+	
+	cout << "Unijeli ste broj " << broj << "." << endl;
+	
+	return 0;
+}
