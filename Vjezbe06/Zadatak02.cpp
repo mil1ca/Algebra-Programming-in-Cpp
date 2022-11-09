c703f288869f4516ca0db6c3fb22745ebac598b3
MĐ <117772875+mil1ca@users.noreply.github.com>
2022-11-09 21:09:19 +0100

    Create Zadatak02.cpp

---
+++ Vjezbe06/Zadatak02.cpp
@@ -0,0 +1,29 @@
+/*Napišite program koji će stvoriti polje sa svim brojevima između 10 i 100 koju su djeljivi s 3. 
+Ispišite to polje u obrnutom redoslijedu i izračunajte sumu brojeva u polju.*/ 
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(void) {
+    
+    vector<int> poljeBrojeva;
+    int suma = 0;
+    
+    for (int i = 10; i <= 100; i++) {
+        if (i % 3 == 0) {
+            poljeBrojeva.push_back(i);
+        }
+    }
+    cout << "Brojevi iz raspona [10-100] djeljivi s 3:" << endl;
+    
+    for (int i = poljeBrojeva.size() - 1; i >= 0; i--) {
+        cout << poljeBrojeva[i] << " ";
+        suma += poljeBrojeva[i];
+    }
+    cout << endl;
+    cout << "Suma tih brojeva je " << suma << "." << endl;
+    cout << endl;
+    return 0;
+    
+}
