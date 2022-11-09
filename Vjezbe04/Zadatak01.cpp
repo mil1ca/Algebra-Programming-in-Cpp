f6d543ed126e20c0e737a04b94bd4c9fe852623c
MĐ <117772875+mil1ca@users.noreply.github.com>
2022-11-09 15:50:38 +0100

    Create Zadatak01.cpp

---
+++ Vjezbe04/Zadatak01.cpp
@@ -0,0 +1,46 @@
+/*Napišite program koji s tipkovnice učitava dva cijela broja i operaciju.
+Ispravne operacije su +, --, * , /. Ispišite*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int brojA, brojB;
+	char racunskaOperacija;
+	bool ispravnoUnesenaRacunskaOperacija = true;
+	int rezultat;
+	
+	cout << "Unesite prvi broj > ";
+	cin >> brojA;
+	cout << "Unesite drugi broj > ";
+	cin >> brojB;
+	
+	cout << "Unesite znak operacije (+, -, *, /) > ";
+	cin >> racunskaOperacija;
+	
+	switch (racunskaOperacija) {
+	    case '+':
+	       rezultat = brojA + brojB;
+	       break;
+	    case '-':
+	       rezultat = brojA - brojB;
+	       break;
+	    case '*':
+	       rezultat = brojA * brojB;
+	       break;
+	    case '/':
+	       rezultat = brojA / brojB;
+	       break;
+	    default:
+	       ispravnoUnesenaRacunskaOperacija = false;
+	       cout << "Neispravno unesena racunska operacija." << endl;
+	       break;
+	}
+	
+	if (ispravnoUnesenaRacunskaOperacija) {
+	    cout << brojA << " " << racunskaOperacija << " " << brojB << " = " << rezultat << endl;
+	}
+	
+	return 0;
+}
