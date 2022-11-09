07b9aad572f06c65e0b0eb344c2dbdfc47a5b20e
MĐ <117772875+mil1ca@users.noreply.github.com>
2022-11-09 17:35:27 +0100

    Create Zadatak03.cpp

---
+++ Vjezbe05/Zadatak03.cpp
@@ -0,0 +1,21 @@
+/*Napišite program koji za svaki broj iz polja ispisuje je li on paran ili neparan.*/
+
+#include <iostream>
+using namespace std;
+
+int main(void) {
+
+	int polje[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	
+	for (int i = 0; i < 10; i++) {
+	    if (polje[i] % 2 == 0) {
+	        cout << polje[i] << " je paran." << endl;
+	    } else {
+	        cout << polje[i] << " je neparan." << endl;
+	    }
+	}
+	
+	cout << endl;
+	
+	return 0;
+}
