8ce80ca43a148a28f268a37b1030895c6cdd1e7a
MĐ <117772875+mil1ca@users.noreply.github.com>
2022-11-09 15:23:38 +0100

    Create Zadatak11.cpp

---
+++ Vjezbe03/Zadatak11.cpp
@@ -0,0 +1,31 @@
+/*Napišite program koji s tipkovnice učitava jedan broj te ispisuje je li on prost.*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int broj;
+	
+	cout << "Unesite broj > ";
+	cin >> broj;
+	
+	bool brojJeProst = true;
+	
+	for (int i = 2; i < broj; i++) {
+	    if (broj % i == 0) {
+	        brojJeProst = false;
+	        break;
+	    } else {
+	        brojJeProst = true;
+	    }
+	}
+	
+	if (brojJeProst) {
+	    cout << "Broj " << broj << " je prost." << endl;
+	} else {
+	    cout << "Broj " << broj << " nije prost." << endl;
+	}
+	
+	return 0;
+}
