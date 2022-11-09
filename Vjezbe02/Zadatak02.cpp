c7a3faaa6ec706ed95380b81b2bdc78ea77d3f63
MĐ <117772875+mil1ca@users.noreply.github.com>
2022-11-09 14:12:30 +0100

    Create Zadatak02.cpp

---
+++ Vjezbe02/Zadatak02.cpp
@@ -0,0 +1,13 @@
+/*Napišite program koji ispisuje tri riječi: “ Hello ”, “World”, “C++”, svaku u svom retku, te razdvojene s praznim retkom.*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	cout << "Hello" << endl;
+	cout << "World" << endl;
+	cout << "C++" << endl;
+
+	return 0;
+}
