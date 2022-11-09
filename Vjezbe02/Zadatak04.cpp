4b831c7556a954c9a1b6c5745d2bc711fbb605d4
MĐ <117772875+mil1ca@users.noreply.github.com>
2022-11-09 13:26:02 +0100

    Create Zadatak04.cpp

---
+++ Vjezbe02/Zadatak04.cpp
@@ -0,0 +1,20 @@
+/*Napišite program koji za dani dvoznamenkasti broj ispisuje njegovu lijevu i desnu znamenku. 
+Svaka znamenka treba biti ispisana u zasebnom retku. Broj unesite direktno u programu.*/
+
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    
+    int broj = 20;
+    
+    int lijevaZnamenka, desnaZnamenka;
+    lijevaZnamenka = broj / 10;
+    desnaZnamenka = broj % 10;
+    
+    cout << "Broj: " << broj << endl;
+    cout << "Lijeva znamenka: " << lijevaZnamenka << endl;
+    cout << "Desna znamenka: " << desnaZnamenka << endl;
+    
+    return 0;
+}
