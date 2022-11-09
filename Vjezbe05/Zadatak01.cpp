c0d4507bf2dea9b9c042734ec8e9f9b8d5616925
MĐ <117772875+mil1ca@users.noreply.github.com>
2022-11-09 23:43:01 +0100

    Update Zadatak01.cpp

--- Vjezbe05/Zadatak01.cpp
+++ Vjezbe05/Zadatak01.cpp
@@ -4,15 +4,13 @@
 #include <iostream>
 using namespace std;
 
-int main(void) {
+int main() {
     
     int polje[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     
     for (int i = 0; i < 10; i++) {
         cout << polje[i] << " ";
     }
-    
-    cout << endl;
     
     return 0;
 }
