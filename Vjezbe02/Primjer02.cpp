f63c1bc619fb54c5ea4e1e4489687f3a1509b592
MĐ <117772875+mil1ca@users.noreply.github.com>
2022-11-09 14:16:30 +0100

    Create Primjer02.cpp

---
+++ Vjezbe02/Primjer02.cpp
@@ -0,0 +1,15 @@
+/*Napišite program koji ispisuje poruku: 
+
+Dobar dan
+
+Dobro jutro*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	cout << endl << "Dobar dan" << endl << "Dobro jutro";
+
+	return 0;
+}
