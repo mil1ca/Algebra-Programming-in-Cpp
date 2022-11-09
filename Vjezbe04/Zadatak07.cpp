9f1d275d706d081f3db0a86150efab8ba4b3a8f1
MĐ <117772875+mil1ca@users.noreply.github.com>
2022-11-09 17:19:00 +0100

    Create Zadatak07.cpp

---
+++ Vjezbe04/Zadatak07.cpp
@@ -0,0 +1,36 @@
+/*Program računa srednju ocjenu uspjeha učenika. Nakon posljednje ocjene treba unijeti 0. U slučaju da je bilo koja od ocjena 1,
+ispisuje se poruka: Negativan uspjeh. Ako se unese vrijednost koja nije iz raspona od 1 5, ispisat će se poruka: Pogrešan unos.*/
+
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    
+    float ocjena, brojac = 0, suma = 0, srednjaOcjena;
+    
+    cout << "Za prekid unosa ocjena unesite 0." << endl;
+    
+    do {
+        cout << "Unesite ocjenu > ";
+        cin >> ocjena;
+        
+        if (ocjena == 1) {
+            cout << "Negativan uspjeh." << endl;
+            return 0;
+        } else if (ocjena < 0 || ocjena > 5) {
+            cout << "Pogresan unos." << endl;
+            return 0;
+        } else {
+            suma += ocjena;
+            if (ocjena != 0) {
+                brojac++;
+            }
+        }
+    } while (ocjena > 0);
+    
+    srednjaOcjena = suma / (1.0 * brojac);
+    
+    cout << "Srednja ocjena ucenika je " << srednjaOcjena << "." << endl;
+    
+	return 0;
+}
