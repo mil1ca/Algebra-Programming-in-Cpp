ce849f8597191e9c29efa04b658f9ffae25b35bc
MĐ <117772875+mil1ca@users.noreply.github.com>
2022-11-09 15:05:06 +0100

    Create Zadatak08.cpp

---
+++ Vjezbe03/Zadatak08.cpp
@@ -0,0 +1,29 @@
+/*Napišite program koji s tipkovnice učitava dva cijela broja te ispisuje sve
+brojeve od većeg prema manjem.*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int veci, manji;
+	
+	cout << "Unesite prvi broj > ";
+	cin >> veci;
+	cout <<"Unesite drugi broj > ";
+	cin >> manji;
+	
+	int temp;
+	if (manji > veci) {
+	    temp = manji;
+	    manji = veci;
+	    veci = temp;
+	}
+	
+	while (veci >= manji) {
+	    cout << veci << " ";
+	    veci--;
+	}
+	
+	return 0;
+}
