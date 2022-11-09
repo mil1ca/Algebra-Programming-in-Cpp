a7060beb42cfbcb9dd4721a009c1481960f72b48
MĐ <117772875+mil1ca@users.noreply.github.com>
2022-11-09 15:55:42 +0100

    Create Zadatak02.cpp

---
+++ Vjezbe04/Zadatak02.cpp
@@ -0,0 +1,43 @@
+/*Napišite program koji učitava redni broj dana u tjednu (1 7), te ispisuje puni naziv dana.*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int redniBrojDana;
+	
+	cout << "Unesite redni broj dana u tjednu > ";
+	cin >> redniBrojDana;
+	
+	switch (redniBrojDana) {
+	    case 1:
+	       cout << "Ponedjeljak.";
+	       break;
+	    case 2:
+	       cout << "Utorak.";
+	       break;
+	    case 3:
+	       cout << "Srijeda.";
+	       break;
+	    case 4:
+	       cout << "Cetvrtak.";
+	       break;
+	    case 5:
+	       cout << "Petak.";
+	       break;
+	    case 6:
+	       cout << "Subota.";
+	       break;
+	    case 7:
+	       cout << "Nedjelja.";
+	       break;
+	    default:
+	       cout << "Neispravan redni broj.";
+	       break;
+	}
+	
+	cout << endl;
+	
+	return 0;
+}
