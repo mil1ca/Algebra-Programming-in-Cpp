c9d3d3f590331c9eba871371c38c30b880966709
MĐ <117772875+mil1ca@users.noreply.github.com>
2022-11-09 14:19:13 +0100

    Create Primjer03.cpp

---
+++ Vjezbe02/Primjer03.cpp
@@ -0,0 +1,15 @@
+/*Deklarirajte varijablu broj koja je tipa cijeli broj, te u nju pospremite vrijednost 52. 
+Ispišite varijablu broj na ekran.*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int broj;
+	broj = 52;
+	
+	cout << "U varijablu broj pohranjena je vrijednost " << broj << "." << endl;
+
+	return 0;
+}
