c606a9e9db9c41e2e47e1939b4ed00270fe7d7f2
MĐ <117772875+mil1ca@users.noreply.github.com>
2022-11-09 15:16:57 +0100

    Create Zadatak10.cpp

---
+++ Vjezbe03/Zadatak10.cpp
@@ -0,0 +1,23 @@
+/*Napišite program koji s tipkovnice učitava dva cijela broja A i B, te ispisuje pravokutnik sastavljen od znaka * koji je širine A i visine B.*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int visina, sirina;
+	
+	cout << "Unesite visinu pravokutnika > ";
+	cin >> visina;
+	cout << "Unesite sirinu pravokutnika > ";
+	cin >> sirina;
+	
+	for (int i = 1; i <= visina; i++) {
+	    for (int j = 1; j <= sirina; j++) {
+	        cout << " * ";
+	    }
+	    cout << endl;
+	}
+  
+	return 0;
+}
