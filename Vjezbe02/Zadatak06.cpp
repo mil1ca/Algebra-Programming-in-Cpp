e1d650b922d6391eb0dfe11783f686251847333f
MĐ <117772875+mil1ca@users.noreply.github.com>
2022-11-09 13:34:22 +0100

    Create Zadatak06.cpp

---
+++ Vjezbe02/Zadatak06.cpp
@@ -0,0 +1,17 @@
+/*Napišite program koji za dani dvoznamenkasti broj (unesen direktno u programu) ispisuje njegov “obrnuti” broj. 
+Npr. za broj 42, potrebno je ispisati 24.*/
+
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    
+    int broj = 25;
+    
+    int obrnutiBroj = ((broj % 10) * 10) + (broj / 10);
+    
+    cout << "Broj: " << broj << endl;
+    cout << "Obrnuti broj: " << obrnutiBroj << endl;
+    
+    return 0;
+}
