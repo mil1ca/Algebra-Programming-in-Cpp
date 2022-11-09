a02eb30b626dafdeed72a8aaa09f9fc6bbeaec6a
MĐ <117772875+mil1ca@users.noreply.github.com>
2022-11-09 22:05:43 +0100

    Create Zadatak06.cpp

---
+++ Vjezbe06/Zadatak06.cpp
@@ -0,0 +1,55 @@
+/*Napišite program koji učitava od korisnika broj brojeva koje misli učitati. 
+Nakon unosa parne brojeve upisuje u polje parniBrojevi , a neparne u polje neparniBrojevi. 
+Ispišite polje parnih u normalnom, a polje neparnih u obrnutom redoslijedu.*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(void) {
+    
+    int broj;
+    cout << "Unesite koliko brojeva zelite upisati > ";
+    cin >> broj;
+    
+    int polje[broj];
+    
+    for (int i = 0; i < broj; i++) {
+        cout << "Upisite " << i + 1 << ". broj > ";
+        cin >> polje[i];
+    }
+    
+    vector<int> parni;
+    vector<int> neparni;
+    
+    for (int i = 0; i < broj; i++) {
+        if (polje[i] % 2 == 0) {
+            parni.push_back(polje[i]);
+        } else {
+            neparni.push_back(polje[i]);
+        }
+    }
+    cout << "Parni brojevi:" << endl;
+    for (int i = 0; i < parni.size(); i++) {
+        cout << parni[i];
+        if (i == parni.size() - 1) {
+            cout << ".";
+        } else {
+            cout << ", ";
+        }
+    }
+    cout << endl;
+    cout << "Neparni brojevi:" << endl;
+    for (int i = neparni.size() - 1; i >= 0; i--) {
+        cout << neparni[i];
+        if (i == 0) {
+            cout << ".";
+        } else {
+            cout << ", ";
+        }
+    }
+    cout << endl;
+    
+    return 0;
+    
+}
