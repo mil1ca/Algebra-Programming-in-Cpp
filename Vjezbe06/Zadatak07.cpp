410aab6fe9140e233e0dbd0e3a17ec80aadaa702
MĐ <117772875+mil1ca@users.noreply.github.com>
2022-11-09 22:29:47 +0100

    Create Zadatak07.cpp

---
+++ Vjezbe06/Zadatak07.cpp
@@ -0,0 +1,53 @@
+/*Učitajte pozitivni broj od korisnika. Ukoliko je uneseni broj negativan
+ispišite grešku "Morate unijeti broj veći ili jednak 10." i tražite ponovni
+unos.
+Stvorite polje prostih brojeva od 10 do n i ispišite elemente tog polja
+odvojene zarezom.*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(void) {
+    
+    int broj;
+    
+    do {
+        cout << "Unesite broj > ";
+        cin >> broj;
+        
+        if (broj < 10) {
+            cout << "Morate unijeti broj veci ili jednak 10." << endl;
+        }
+        
+    } while (broj < 10);
+    
+    vector<int> poljeProstihBrojeva;
+    bool prostBroj;
+    
+    for (int i = 10; i < broj; i++) {
+        for (int j = 2; j < i; j++) {
+            if (i % j == 0) {
+                prostBroj = false;
+                break;
+            } else {
+                prostBroj = true;
+            }
+        }
+        if (prostBroj) {
+            poljeProstihBrojeva.push_back(i);
+        }
+    }
+    
+    for (int i = 0; i < poljeProstihBrojeva.size(); i++) {
+        cout << poljeProstihBrojeva[i];
+        if (i == poljeProstihBrojeva.size() - 1) {
+            cout << ".";
+        } else {
+            cout << ", ";
+        }
+    }
+  
+    return 0;
+    
+}
