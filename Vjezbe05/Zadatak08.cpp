142bbe36b58fc6f7eeff02f4ca03cf9e2d1d063e
MĐ <117772875+mil1ca@users.noreply.github.com>
2022-11-09 18:28:21 +0100

    Create Zadatak08.cpp

---
+++ Vjezbe05/Zadatak08.cpp
@@ -0,0 +1,36 @@
+/*Napišite program koji će u polju od 20 elemenata pronaći najveći i najmanji element. Ispišite te
+elemente. Polje popunite slučajnim brojevima.*/
+
+#define SIZE 20
+
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main(void) {
+    
+    int polje[SIZE];
+    
+    for (int i = 0; i < SIZE; i++) {
+        polje[i] = rand() % 100;
+    }
+    
+    for (int i = 0; i < SIZE; i++) {
+        cout << polje[i] << endl;
+    }
+    
+    int najmanji = polje[0], najveci = polje[0];
+    for (int i = 0; i < SIZE; i++) {
+        if (polje[i] < najmanji) {
+            najmanji = polje[i];
+        }
+        if (polje[i] > najveci) {
+            najveci = polje[i];
+        }
+    }
+    
+    cout << "Najmanji broj je " << najmanji << "." << endl;
+    cout << "Najveci broj je " << najveci << "." << endl;
+	
+	return 0;
+}
