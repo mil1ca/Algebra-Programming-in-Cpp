131ef7eb6cfbf28b60ede3767696a3c4dfba95b6
MĐ <117772875+mil1ca@users.noreply.github.com>
2022-11-09 17:41:38 +0100

    Create Zadatak04.cpp

---
+++ Vjezbe05/Zadatak04.cpp
@@ -0,0 +1,27 @@
+/*Napišite program koji s tipkovnice učitava 10 brojeva u polje, te ispišite to polje
+od kraja prema početku odvojene zarezom. Nakon zadnjeg ispisanog broja slijedi točka.*/
+
+#define SIZE 10
+
+#include <iostream>
+using namespace std;
+
+int main(void) {
+
+	int polje[SIZE];
+	
+	for (int i = 0; i < SIZE; i++) {
+	    cout << "Upisite " << i + 1 << ". broj > ";
+	    cin >> polje[i];
+	}
+	
+	for (int i = SIZE - 1; i >= 0; i--) {
+	    cout << polje[i];
+	    if (i == 0) {
+	        cout << ".";
+	    } else {
+	        cout << ", ";
+	    }
+	}
+	return 0;
+}
