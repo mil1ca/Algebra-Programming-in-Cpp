5e14c02ca0610847c928856536f71f4e7b89d03b
MĐ <117772875+mil1ca@users.noreply.github.com>
2022-11-09 15:39:08 +0100

    Create Zadatak12.cpp

---
+++ Vjezbe03/Zadatak12.cpp
@@ -0,0 +1,40 @@
+/*Napišite program koji s tipkovnice učitava dva cijela broja te ispisuje sve proste brojeve između njih.*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int prvi, drugi;
+	
+	cout << "Unesite prvi broj > ";
+	cin >> prvi;
+	cout << "Unesite drugi broj > ";
+	cin >> drugi;
+	
+	bool brojJeProst = true;
+	int temp;
+	
+	//uzlazni redoslijed
+	if (drugi < prvi) {
+	    temp = drugi;
+	    drugi = prvi;
+	    prvi = temp;
+	} 
+	
+	for (int i = prvi; i < drugi; i++) {
+	    for (int j = 2; j < i; j++) {
+	        if (i % j == 0) {
+	            brojJeProst = false;
+	            break;
+	        } else {
+	            brojJeProst = true;
+	        }
+	    }
+	    if (brojJeProst) {
+	        cout << i << " ";
+	    }
+	} 
+	cout << endl;
+	return 0;
+}
