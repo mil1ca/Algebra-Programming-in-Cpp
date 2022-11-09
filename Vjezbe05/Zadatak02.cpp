e486cd04f2a3945d7aa27267210637ee24cd44b8
MĐ <117772875+mil1ca@users.noreply.github.com>
2022-11-09 17:30:12 +0100

    Create Zadatak02.cpp

---
+++ Vjezbe05/Zadatak02.cpp
@@ -0,0 +1,18 @@
+/*Napišite program kao u prethodnom zadatku, samo elemente ispišite od kraja
+prema početku odvojene zarezom. Nakon zadnjeg broja slijedi točka.*/
+
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    
+    int polje[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    
+    for (int i = 10 - 1; i >= 0; i--) {
+        cout << polje[i] << " ";
+    }
+    
+    cout << endl;
+    
+    return 0;
+}
