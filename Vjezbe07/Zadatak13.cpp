f21a24e1ffd2f63b2416a97980eb8a503853c04f
MĐ <117772875+mil1ca@users.noreply.github.com>
2022-11-10 16:57:38 +0100

    Create Zadatak13.cpp

---
+++ Vjezbe07/Zadatak13.cpp
@@ -0,0 +1,32 @@
+// Napišite funkciju koja vraća sumu parnih brojeva između 100 i 200.
+
+#define MIN 100
+#define MAX 200
+
+#include <iostream>
+using namespace std;
+
+
+// Funkcija koja vraća sumu od 1 do primljenog broja
+int suma_parnih(int x, int y) 
+{
+    int suma = 0;
+    
+    for (int i = x; i <= y; i++) 
+    {
+        if (i % 2 == 0) 
+        {
+            suma += i;
+        }
+    }
+    
+    return suma;
+} 
+
+int main(void) 
+{
+    
+    cout << "Suma parnih brojeva izmedju " << MIN << " i " << MAX << " je " << suma_parnih(MIN, MAX) << "." << endl;
+    
+    return 0;
+}
