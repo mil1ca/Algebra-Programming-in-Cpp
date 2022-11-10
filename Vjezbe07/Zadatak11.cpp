1044b1b1d84c585daba1ed81f170e226f4d4ff6b
MĐ <117772875+mil1ca@users.noreply.github.com>
2022-11-10 16:45:24 +0100

    Create Zadatak11.cpp

---
+++ Vjezbe07/Zadatak11.cpp
@@ -0,0 +1,50 @@
+// Napišite funkciju koja vraća istinu ako je proslijeđeni broj palindroman.
+
+#include <iostream>
+using namespace std;
+
+
+// Funkcija koja vraća obrnuti broj danog cijelog broja
+bool palindrome(int number) 
+{
+    bool is_palindrome = false;
+    int reverse = 0;
+    int num = number;
+    
+    // Obrnuti redoslijed znamenke
+    while (number != 0) 
+    {
+        reverse = (reverse * 10) + (number % 10);
+        number /= 10;
+    }
+    
+    // Usporedba
+    if (num == reverse) 
+    {
+        is_palindrome = true;
+    }
+    
+    return is_palindrome;
+} 
+
+
+int main(void) 
+{
+    int broj;
+    
+    cout << "Unesite broj da provjerite da li je palindroman > ";
+    cin >> broj;
+    
+    bool jeste_palindroman = palindrome(broj);
+    
+    if (jeste_palindroman) 
+    {
+        cout << "Broj je palindroman." << endl;
+    } 
+    else 
+    {
+        cout << "Broj nije palindroman." << endl;
+    }
+    
+    return 0;
+}
